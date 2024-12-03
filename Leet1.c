d1903f6c5d421269b1ca367d605728491fcd406f
João Vitor Montanher Lago <jvmlago@inf.ufpel.edu.br>
2024-12-03 01:02:02 -0300

    Exercícios 1 e 2 do LeetCode

---
+++ Leet1.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* saida = (int*)malloc(2*sizeof(int));
+    *returnSize = 2;
+    if (saida == NULL){
+    printf("Erro\n");
+    return NULL;
+    }
+    for(int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if(nums[i] + nums[j] == target){
+                saida[0] = i;
+                saida[1] = j;
+                return saida;
+            }
+        }
+    }
+    return NULL;
+}
---
+++ Leet2.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* aux = (struct ListNode*) malloc(sizeof(struct ListNode));
+    struct ListNode* soma = aux;
+    aux->next=NULL;
+    int lista1=0, lista2=0, v1=0;
+    do{
+        if (l1 != NULL) {
+            lista1 = l1->val;
+            l1 = l1->next;
+        }
+        else lista1 = 0;
+        if (l2 != NULL) {
+            lista2 = l2->val;
+            l2 = l2->next;
+        }
+        else lista2 = 0;
+        aux->val = lista1 + lista2 + v1;
+        v1=0;
+        if(aux->val >= 10){
+            aux->val = aux->val-10;
+            v1++;
+        }
+        if(l1 != NULL || l2 != NULL || v1 !=0)
+        {
+            aux->next=malloc(sizeof(struct ListNode)); 
+            aux->next->next=NULL;
+            aux = aux->next;
+        }
+    }while(l1 != NULL || l2 != NULL || v1 !=0);
+    return soma;
+}
