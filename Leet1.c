29d6b6a12e4b34cf9f33776df81bdab5525c63be
João Vitor Montanher Lago <jvmlago@inf.ufpel.edu.br>
2024-12-07 00:26:24 -0300

    arrumei

--- Leet1.c
+++ Leet1.c
@@ -2,6 +2,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 #include <stdio.h>
+#include <stdlib.h>
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* saida = (int*)malloc(2*sizeof(int));
     *returnSize = 2;
